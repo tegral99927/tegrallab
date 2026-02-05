e00d384f51bdd256b07e94d0e7b50baeda3ca9e6
tegral <128121101+tegral99927@users.noreply.github.com>
2026-02-05 20:58:36 +0900

    feat: Implement Google Analytics and add new content pages for tools, games, about, policy, and contact.

---
+++ asset/js/gtag.js
@@ -0,0 +1,13 @@
+// Google Analytics (GA4) Initialization
+(function () {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-X3NZ7HYK3F';
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-X3NZ7HYK3F');
+})();
